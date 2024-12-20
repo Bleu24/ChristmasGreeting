f2173e3779c3e5914176ece5d9ff024ffcd7a418
Bleu24 <bryanreamill2403@gmail.com>
2024-12-20 19:48:03 +0800

    added text

--- app/src/main/java/com/reamillo/christmasgreetingapp/MainActivity.kt
+++ app/src/main/java/com/reamillo/christmasgreetingapp/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,11 +49,11 @@ fun ChristmasMessage(onNext: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
             .background(Color(0xFF0B6623)) // Christmas green background
+            .padding(25.dp)
     ) {
         Text(
-            text = "Merry Christmas!",
+            text = "Happy Holidays!",
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold,
             color = Color.White,
@@ -63,7 +65,7 @@ fun ChristmasMessage(onNext: () -> Unit) {
             painter = painterResource(id = R.drawable.christmas_image),
             contentDescription = "Christmas Image",
             modifier = Modifier
-                .height(200.dp)
+                .height(600.dp)
                 .align(Alignment.CenterHorizontally)
         )
         Spacer(modifier = Modifier.height(20.dp))
@@ -84,7 +86,11 @@ fun ChristmasMessage(onNext: () -> Unit) {
             onClick = onNext,
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
-                .padding(16.dp)
+                .padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFB22222), // Christmas red button
+                contentColor = Color.White
+            ) // Christmas red button
         ) {
             Text("Open Dedication Message")
         }
@@ -96,8 +102,8 @@ fun DedicationMessage(onBack: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
             .background(Color(0xFFB22222)) // Christmas red background
+            .padding(16.dp)
     ) {
         Text(
             text = "Dedication Message",
@@ -107,19 +113,54 @@ fun DedicationMessage(onBack: () -> Unit) {
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
         )
-        Spacer(modifier = Modifier.height(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.christmas_cat),
+            contentDescription = "Christmas Cat",
+            modifier = Modifier
+                .height(400.dp)
+                .align(Alignment.CenterHorizontally)
+                .size(800.dp)
+        )
         Text(
-            text = "Merry Christmas Ceanne!",
+            text = "Happy Holidays to my dearest Ceanne!",
             fontSize = 24.sp,
             color = Color.White,
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier = Modifier.height(20.dp))
         Text(
-            text = "I hope you have a wonderful Christmas and a Happy New Year!",
-            fontSize = 24.sp,
+            text = "I hope you have a wonderful Pasasalamat and a Happy New Year!" +
+                    " Anyways, here's a cute cat for you!",
+            fontSize = 16.sp,
             color = Color.White,
-            textAlign = TextAlign.Center,
+            textAlign = TextAlign.Justify,
+            fontFamily = FontFamily.Serif,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "Okay, time for a heartwarming message!!! 💖" +
+                    " I've been always grateful for you ever since we met. " +
+                    "You've been a great partner and I'm so lucky to have you in my life. " +
+                    "After hardships and challenges, we've been through, " +
+                    "I'm glad that we're still here for each other. " ,
+            fontSize = 16.sp,
+            color = Color.White,
+            textAlign = TextAlign.Justify,
+            fontFamily = FontFamily.Serif,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text =  "No words and sentences can fit here in this screen " +
+                    "On how much I appreciate you. " +
+                    "The reason why I put this cat because this is the epitome of us and our relationship" +
+                    "I hope you like it and I hope you like the message. " ,
+            fontSize = 16.sp,
+            color = Color.White,
+            textAlign = TextAlign.Justify,
+            fontFamily = FontFamily.Serif,
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(20.dp))
@@ -129,7 +170,7 @@ fun DedicationMessage(onBack: () -> Unit) {
                 .align(Alignment.CenterHorizontally)
                 .padding(16.dp)
         ) {
-            Text("Back to Christmas Message")
+            Text("Back to Cover")
         }
     }
 }
